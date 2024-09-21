3c5a0a490e882f4e1fafc4cf9e7f4ff8165bd986
rawanst <rawantabaja12@gmail.com>
2024-09-21 14:08:41 +0200

    OK: Blog + List component avec une liste d'articles en dure

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -18,35 +18,83 @@ const Blog = () => {
         obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
         nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
         tenetur error, harum nesciunt ipsum debitis quas aliquid.`
+    },
+    {
+      id: '2',
+      titre: 'Un titre 2',
+      date: "2024-09-18",
+      resume: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur`,
+      contenu: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+        optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+        obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+        nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+        tenetur error, harum nesciunt ipsum debitis quas aliquid.`
+    },
+    {
+      id: '3',
+      titre: 'Un titre 3',
+      date: "2024-09-18",
+      resume: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur`,
+      contenu: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+        optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+        obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+        nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+        tenetur error, harum nesciunt ipsum debitis quas aliquid.`
     }
   ]
 
+  
+
   return (
-    <>
+    <div
+      style={{
+        marginTop: '-4%',
+      }}
+    >
       <Header />
-      <FrontPage title="Blog" content=""/>
+      <FrontPage 
+        title="Blog" 
+        content='Passionné par la tech, je transforme les défis en solutions innovantes. 
+          Découvrez mon parcours et mes réalisations.'
+      />
       <div
         style={{
           display: 'flex',
-          flexDirection: 'row',
+          flexDirection: 'column',
           alignItems: 'center',
           lexWrap: 'wrap',
           justifyContent: 'space-evenly',
-          marginTop: '5%',
-          marginBottom: '5%',
+          marginTop: '4%',
+          marginBottom: '4%',
           marginLeft: '0.5%',
         }}
       >
-        <Liste 
+        {articles.map((article, i) => {
+          return (
+            <Liste 
+              key={article.id}
+              id={article.id}
+              titre={article.titre}
+              date={article.date}
+              resume={article.resume}
+              contenu={article.contenu}
+            />
+          )
+        })}
+        {/* <Liste 
           id={articles[0].id}
           titre={articles[0].titre}
           date={articles[0].date}
           resume={articles[0].resume}
           contenu={articles[0].contenu}
-        />
+        /> */}
       </div>
       <Footer />
-    </>
+    </div>
   )
 }
 
