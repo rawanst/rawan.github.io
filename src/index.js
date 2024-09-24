08824e687686a46d919affc7323fae7349a5b6dc
rawanst <rawantabaja12@gmail.com>
2024-09-24 18:54:01 +0200

    HasRouter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import * as React from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom"
-import App from './App'
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
+import App from './App';
 import Blog from './Pages/Blog'
 import Portfolio from './Pages/Portfolio'
 import Article from './Pages/Article'
@@ -12,44 +12,40 @@ import ComingSoon from './Pages/ComingSoon'
 import Home from './Pages/Home'
 import NotFound from './Pages/NotFound'
 
-const basename = process.env.NODE_ENV === 'production' ? '/rawanst.github.io' : '/'
-
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-    },
-    {
-      path: "/home",
-      element: <Home />,
-    },
-    {
-      path: "/blog",
-      element: <Blog />,
-    },
-    {
-      path: "/blog/:id",
-      element: <Article />,
-    },
-    {
-      path: "/portfolio",
-      element: <Portfolio />,
-    },
-    {
-      path: "/portfolio/:id",
-      element: <ComingSoon />,
-    },
-    {
-      path: "/*",
-      element: <NotFound />,
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: basename,
-  }
-);
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/blog",
+    element: <Blog />,
+  },
+  {
+    path: "/blog/:id",
+    element: <Article />,
+  },
+  {
+    path: "/portfolio",
+    element: <Portfolio />,
+  },
+  {
+    path: "/portfolio/:id",
+    element: <ComingSoon />,
+  },
+  {
+    path: "/*",
+    element: <NotFound />,
+  },
+]);
 
-createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+// Le point d'entrée principal de ton application React
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
