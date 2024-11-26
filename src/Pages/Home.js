b16d078d8c2cb0401fe93552bb627149fcdced23
rawanst <rawantabaja12@gmail.com>
2024-11-26 14:09:56 +0100

    FrontPage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,19 +2,41 @@ import FrontPage from '../component/FrontPage'
 import Header from '../component/Header'
 import APropos from '../component/APropos'
 import Footer from '../component/Footer'
+import ShortcutProjets from '../component/ShortcutProjets'
+import ShortcutBlog from '../component/ShortcutBlog'
 
 const Home = () => {
   return(
     <div
       style={{
-        marginTop: '-4%',
+        // marginTop: '-4%',
+        margin: 0,        
       }}
     >
       <Header />
       <FrontPage
-        title='Développeuse Full-Stack'
+        title='Rawan Tabaja'
+        titleDeux='Développeuse Full-Stack'
         content='Passionné par la tech, je transforme les défis en solutions innovantes. 
         Découvrez mon parcours et mes réalisations.'
+        isHomePage={1}
+        links={[
+          {
+            key: 1,
+            name: 'LinkedIn',
+            url :"https://www.linkedin.com/in/rawan-t-5ba1bb170/"
+          },
+          {
+            id: 2,
+            name: 'GitHub',
+            url: "https://github.com/rawanst",
+          },
+          {
+            id: 3,
+            name: 'rawantabaja12@gmail.com',
+            url:"mailto:rawantabaja12@gmail.com?cc=&subject=Hello world !&body=Bonjour Rawan, ",
+          },
+        ]}
       />
       <APropos />
       <Footer />
--- src/component/FrontPage.js
+++ src/component/FrontPage.js
@@ -1,50 +1,102 @@
-const FrontPage = ({title, content}) => {
+import React from 'react'
+import {
+  Box,
+  Container,
+  Typography,
+  Divider,
+  Chip
+} from "@mui/material"
+import Grid from '@mui/material/Grid2'
+
+const FrontPage = ({title, titleDeux, content, isHomePage, links}) => {
 
   return(
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-        alignContent: 'center',
-
-        width: '98%',
-        borderRadius: '0 0 1em 1em',
-        marginTop: '-8%',
-        marginLeft: '1%',
-        paddingTop: '12%',
-        paddingBottom: '5%',
-
-        color: '#e8e3edff',
-        fontSize: 'calc(10px + 2vmin)',
-        backgroundColor: '#823ccbff',
-      }}
+    <Box
+      height= '90vh'
+      bgcolor= '#823ccbff'
     >
-      <h1
-        style={{
-          fontFamily: "Melodrama-Bold", 
-          fontWeight: '500',
-          fontSize: 'xxx-large',
-          margin: '0',
-          marginLeft: '1%',
+      <Container
+        sx={{
+          height: '90vh',
+          diplay: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+          alignItems: 'center',
+          alignContent: 'center',
         }}
       >
-        {title}
-      </h1>
-      <p 
-        style={{
-          fontFamily: "Melodrama-Light", 
-          fontWeight: '300',
-          margin: '0',
-          marginLeft: '1%',
-          paddingRight: '45%',
-          textAlign: 'left',
-          fontSize: 'x-large',
-        }}
-      >
-        {content}
-      </p>
-    </div>
+        <Grid container spacing={2}>
+          <Grid 
+            size={{
+              xs: 12,
+              sm: 12,
+              md: 7,
+              lg: 7,
+            }}
+          >
+            <Typography
+              variant= 'h2'
+              sx={{
+                fontFamily: "Meloriac-Regular",
+                color: '#b9cb3c',
+                wordWrap: 'break-word'
+              }}
+            >
+              {title}
+            </Typography>
+            
+            {titleDeux && 
+              <Typography
+              variant= 'h2'
+              sx={{
+                fontFamily: 'Melodrama-Light', 
+                color: '#b9cb3c',
+                wordWrap: 'break-word'
+              }}
+              >
+                {titleDeux}
+              </Typography>
+            }
+
+            <Divider sx={{bgcolor:'#c2cf69', opacity: '0.4'}}/>
+            <Typography 
+              variant= 'h5'
+              sx={{
+                fontFamily: "Melodrama-Light", 
+                color: '#c2cf69',
+                wordWrap: 'break-word'
+              }}
+            >
+              {content}
+            </Typography>
+
+            {isHomePage && links && links.map(link => 
+              <Chip 
+                key={link.id}
+                label={link.name} 
+                variant="outlined" 
+                sx={{
+                  m: 0.5,
+                  bgcolor:'#c2cf69',
+                  fontFamily: "Melodrama", 
+                  color: '#823ccbff',
+                  ':hover': { 
+                    color: '#e8e3edff'
+                  },
+                }}
+                onClick={
+                  () => {
+                  window.open(link.url, '_blank')
+                }}
+              />
+            )}
+          </Grid>
+        </Grid>
+        
+
+      </Container>
+    </Box>
   )
 }
 
