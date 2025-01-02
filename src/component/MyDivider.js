ad877f289d6fcc848df5e35f5e15e9198f2b84b7
rawanst <rawantabaja12@gmail.com>
2025-01-02 22:14:39 +0100

    INIT MyDivider

---
+++ src/component/MyDivider.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Divider } from "@mui/material"
+
+const MyDivider = () => {
+  return (
+    <Divider
+      sx={{
+        bgcolor:'#b9cb3c',
+        width: '1',
+        mb: '2%'
+      }}
+    />
+  )
+}
+
+export default MyDivider
